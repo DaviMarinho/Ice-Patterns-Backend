36aa8ec1df3580f02926041593f9b61a69e4f404
AlineLermen <aline.helena.lermen@gmail.com>
2023-10-17 16:28:02 -0300

    Dele unecessary

--- src/repository/userAchievement-repository.ts
+++ src/repository/userAchievement-repository.ts
@@ -1,7 +1,4 @@
-import { UpdateResult } from 'typeorm'
 import { dataSource } from '../db/config'
-import { Achievement } from '../db/entities/achievement'
-import { User } from '../db/entities/user'
 import { UserAchievement } from '../db/entities/userAchievement'
 import { UserAchievementsRepository } from './port/userAchievement-repository'
 
@@ -23,80 +20,6 @@ class UserAchievementRepository implements UserAchievementsRepository {
     await this.userAchievementRepository.save(userAchievement)
     return userAchievement
   }
-
-  async findToLogin(email: string): Promise<User> {
-    const userPassword = await this.userRepository.find({
-      where: {
-        email
-      },
-      select: ['password', 'email', 'username', 'name']
-    })
-    return userPassword[0]
-  }
-
-  async findOneByEmail(email: string): Promise<User | undefined> {
-    const user = await this.userRepository.findOneBy({
-      email
-    })
-    if (!user) {
-      return undefined
-    }
-    return user
-  }
-
-  async findOneByUsername(username: string): Promise<User | undefined> {
-    const user = await this.userRepository.findOneBy({
-      username
-    })
-    if (!user) {
-      return undefined
-    }
-    return user
-  }
-
-  async updateUserItemQuantity(username: string, qtCube: number, qtEnergy: number, qtBooster: number): Promise<UpdateResult | undefined> {
-    const result = await this.userRepository.update({
-      username,
-    }, {
-      qtCube,
-      qtEnergy,
-      qtBooster
-    })
-
-    return result
-  }
-
-  async receiveAchievement(username: string, idAchievement: string): Promise<UpdateResult | undefined> {
-
-    const user = await this.userRepository.findOneBy({
-      username
-    })
-    if (!user) {
-      return undefined
-    }
-
-    const achievementsList = user.achievements
-    
-    const newAchievement = await this.achievementRepository.findOneBy({
-      id
-    })
-    if (!newAchievement) {
-      return undefined
-    }
-
-    achievementsList?.push(newAchievement)
-
-    const userAchievement = this.userAchievementRepository.create({
-      username,
-      achievementsId: idAchievement,
-      dateTime
-    })
-
-    await this.userAchievementRepository.save(userAchievement)
-    return userAchievement
-  }
-
-
 }
 
-export default UserRepository
+export default UserAchievementRepository
