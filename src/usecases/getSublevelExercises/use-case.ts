f9394e41d45e580e59f0c8102ed8a2615b3c77de
AlineLermen <aline.helena.lermen@gmail.com>
2023-11-13 18:24:54 -0300

    Missing file for adding alternatives

--- src/usecases/getSublevelExercises/use-case.ts
+++ src/usecases/getSublevelExercises/use-case.ts
@@ -1,9 +1,11 @@
 import { SublevelsRepository } from '../../repository/port/sublevel-repository'
 import { UseCase, UseCaseReponse } from '../domain/use-case'
-import { GetSublevelExercisesResponse } from './domain/getSublevelExercises-response'
+import { DetailedExercise, GetSublevelExercisesResponse } from './domain/getSublevelExercises-response'
 import { GetSublevelExercisesError } from './errors/getSublevelExercises-error'
 import { GetSublevelError } from '../getSublevelContents/errors/getSublevel-error'
 import { ExercisesRepository } from '../../repository/port/exercise-repository'
+import { AlternativesRepository } from '../../repository/port/alternative-repository'
+import { GetAlternativesError } from './errors/getAlternatives-error'
 
 export interface SublevelInfo {
     sublevelId: string
@@ -13,6 +15,7 @@ export class GetSublevelExercisesUseCase implements UseCase<GetSublevelExercises
     constructor(
         private sublevelRepository: SublevelsRepository,
         private exerciseRepository: ExercisesRepository,
+        private alternativeRepository: AlternativesRepository,
     ) {}
 
   async execute({ sublevelId }: SublevelInfo): Promise<UseCaseReponse<GetSublevelExercisesResponse>> {
@@ -31,11 +34,31 @@ export class GetSublevelExercisesUseCase implements UseCase<GetSublevelExercises
 
         const sublevelExercises = await this.exerciseRepository.findBySublevelId(sublevelId)
         
-        if(sublevelExercises) {          
+        if(sublevelExercises) {     
+            const exercisesFormatted: DetailedExercise[] = []
+
+          for (const exercise of sublevelExercises) {
+            const alternatives = await this.alternativeRepository.findByExerciseId(exercise.id)
+
+            if (!alternatives) {
+              return {
+                isSuccess: false,
+                error: new GetAlternativesError()
+              }
+            }
+
+            const missionFormatted: DetailedExercise = {
+              exercise: exercise,
+              alternatives: alternatives
+            }
+
+            exercisesFormatted.push(missionFormatted)
+            }
+
           return { 
               isSuccess: true,
               body: {
-                exercises: sublevelExercises
+                exercises: exercisesFormatted
               }
           }
         } else {
